cacb70527e3073e866a9d6f1444c2950c85a22ba
tepo80 <ahsan.tepo98@gmail.com>
2025-11-16 03:57:38 +0330

    Update cl.py
    bnkmkk

--- cl.py
+++ cl.py
@@ -23,7 +23,7 @@ LINKS_PATH = [
     "https://23.ahsan-tepo20.workers.dev/public?ports=2096,2095,443,8080,8443,2053,2052&stealth=grpc&compression=text&limit=all",
     "https://23.ahsan-tepo20.workers.dev/public?ports=2096,2095,443,8080,8443,2053,2052&stealth=grpc&compression=auto&limit=all",
     "https://23.ahsan-tepo20.workers.dev/public?ports=2096,2095,443,8080,8443,2053,2052&stealth=quic&compression=auto&limit=all",
-    "https://23.ahsan-tepo20.workers.dev/arista?limit=all,
+    "https://23.ahsan-tepo20.workers.dev/arista?limit=all",
     "https://23.ahsan-tepo20.workers.dev/public?ports=2096,2095,443,8080,8443,2053,2052&stealth=websocket&multipath=video,chat,download&compression=json&limit=all",
     "https://23.ahsan-tepo20.workers.dev/public?ports=2096,2095,443,8080,8443,2053,2052&stealth=grpc&multipath=video,chat,download&compression=json&limit=all",
     "https://23.ahsan-tepo20.workers.dev/public?ports=2096,2095,443,8080,8443,2053,2052&stealth=grpc&multipath=video,chat,download&compression=text,json&limit=all",
